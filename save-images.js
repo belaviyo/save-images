b119403b88c9245c9027122fc4ea9b2ac39d0ebe
Joe Ertaba <belaviyo@gmail.com>
2018-05-12 19:30:19 +0430

    fixes #8

--- save-images.js
+++ save-images.js
@@ -14,117 +14,119 @@ window.count = 0;
 // Firefox does not support chrome.downloads.onDeterminingFilename yet
 const diSupport = Boolean(chrome.downloads.onDeterminingFilename);
 
-var download = (() => {
-  let jobs = [];
-  let zip;
-  const indices = {};
-  let tab;
-  let request;
+function Download() {
+  this.zip = new JSZip();
+  this.indices = {};
+}
+Download.prototype.init = function(request, tab) {
+  this.request = request;
+  this.tab = tab;
+  this.jobs = request.images;
 
-  function download(obj) {
-    return new Promise((resolve, reject) => {
-      const id = window.setTimeout(() => reject(Error('fetch timeout')), 10000);
+  this.one();
+};
+Download.prototype.one = function() {
+  const {id, title} = this.tab;
+  const jobs = this.jobs;
+  const request = this.request;
 
-      fetch(obj.src).then(response => {
-        if (response.ok) {
-          const disposition = response.headers.get('Content-Disposition');
-          let name = obj.filename;
-          if (disposition && !name) {
-            const tmp = /filename=([^;]*)/.exec(disposition);
-            if (tmp && tmp.length) {
-              name = tmp[1].replace(/["']$/, '').replace(/^["']/, '');
-            }
+  chrome.browserAction.setBadgeText({
+    tabId: id,
+    text: jobs.length ? String(jobs.length) : ''
+  });
+  chrome.tabs.sendMessage(id, {
+    cmd: 'progress',
+    value: jobs.length
+  });
+  const [j1, j2, j3, j4, j5] = [jobs.shift(), jobs.shift(), jobs.shift(), jobs.shift(), jobs.shift()];
+  if (j1) {
+    Promise.all([
+      j1 ? this.download(j1).catch(() => {}) : Promise.resolve(),
+      j2 ? this.download(j2).catch(() => {}) : Promise.resolve(),
+      j3 ? this.download(j3).catch(() => {}) : Promise.resolve(),
+      j4 ? this.download(j4).catch(() => {}) : Promise.resolve(),
+      j5 ? this.download(j5).catch(() => {}) : Promise.resolve()
+    ]).then(() => this.one());
+  }
+  else {
+    this.zip.generateAsync({type: 'blob'})
+    .then(content => {
+      const time = new Date();
+      let filename = title + ' ' + time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
+      filename = filename
+        .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>{}[\]\\/]/gi, '-');
+      filename += '.zip';
+      const url = URL.createObjectURL(content);
+      chrome.downloads.download({
+        url,
+        filename: request.custom ? request.custom + '/' + filename : filename,
+        conflictAction: 'uniquify',
+        saveAs: request.saveAs
+      }, () => {
+        chrome.tabs.sendMessage(id, {
+          cmd: 'close-me'
+        });
+        window.setTimeout(() => URL.revokeObjectURL(url), 10000);
+      });
+    });
+  }
+};
+Download.prototype.download = function(obj) {
+  return new Promise((resolve, reject) => {
+    const id = window.setTimeout(() => reject(Error('fetch timeout')), 10000);
+
+    const request = this.request;
+    const indices = this.indices;
+
+    fetch(obj.src).then(response => {
+      window.clearTimeout(id);
+      if (response.ok) {
+        const disposition = response.headers.get('Content-Disposition');
+        let name = obj.filename;
+        if (disposition && !name) {
+          const tmp = /filename=([^;]*)/.exec(disposition);
+          if (tmp && tmp.length) {
+            name = tmp[1].replace(/["']$/, '').replace(/^["']/, '');
           }
-          if (!name) {
-            const url = obj.src.replace(/\/$/, '');
-            const tmp = /(title|filename)=([^&]+)/.exec(url);
-            if (tmp && tmp.length) {
-              name = tmp[2];
-            }
-            else {
-              name = url.substring(url.lastIndexOf('/') + 1);
-            }
-            name = decodeURIComponent(name.split('?')[0].split('&')[0]) || 'image';
-          }
-          name = name.slice(-30);
-          if (name.indexOf('.') === -1) {
-            const type = response.headers.get('Content-Type');
-            if (type) {
-              name += '.' + type.split('/').pop().split(/[+;]/).shift();
-            }
-          }
-          if (request.addJPG && name.indexOf('.') === -1) {
-            name += '.jpg';
-          }
-          if (name in indices) {
-            const index = name.lastIndexOf('.') || name.length;
-            const tmp = name.substr(0, index) + ' - ' + indices[name] + name.substr(index);
-            indices[name] += 1;
-            name = tmp;
+        }
+        if (!name) {
+          const url = obj.src.replace(/\/$/, '');
+          const tmp = /(title|filename)=([^&]+)/.exec(url);
+          if (tmp && tmp.length) {
+            name = tmp[2];
           }
           else {
-            indices[name] = 1;
+            name = url.substring(url.lastIndexOf('/') + 1);
           }
-          return response.blob().then(blob => zip.file(name, blob)).then(resolve);
+          name = decodeURIComponent(name.split('?')[0].split('&')[0]) || 'image';
+        }
+        name = name.slice(-30);
+        if (name.indexOf('.') === -1) {
+          const type = response.headers.get('Content-Type');
+          if (type) {
+            name += '.' + type.split('/').pop().split(/[+;]/).shift();
+          }
+        }
+        if (request.addJPG && name.indexOf('.') === -1) {
+          name += '.jpg';
+        }
+        if (name in indices) {
+          const index = name.lastIndexOf('.') || name.length;
+          const tmp = name.substr(0, index) + ' - ' + indices[name] + name.substr(index);
+          indices[name] += 1;
+          name = tmp;
         }
         else {
-          reject(Error('fetch failed'));
+          indices[name] = 1;
         }
-      });
+        return response.blob().then(blob => this.zip.file(name, blob)).then(resolve);
+      }
+      else {
+        reject(Error('fetch failed'));
+      }
     });
-  }
-
-  function one() {
-    chrome.browserAction.setBadgeText({
-      tabId: tab.id,
-      text: jobs.length ? String(jobs.length) : ''
-    });
-    chrome.tabs.sendMessage(tab.id, {
-      cmd: 'progress',
-      value: jobs.length
-    });
-    const [j1, j2, j3, j4, j5] = [jobs.shift(), jobs.shift(), jobs.shift(), jobs.shift(), jobs.shift()];
-    if (j1) {
-      Promise.all([
-        j1 ? download(j1).catch(() => {}) : Promise.resolve(),
-        j2 ? download(j2).catch(() => {}) : Promise.resolve(),
-        j3 ? download(j3).catch(() => {}) : Promise.resolve(),
-        j4 ? download(j4).catch(() => {}) : Promise.resolve(),
-        j5 ? download(j5).catch(() => {}) : Promise.resolve()
-      ]).then(one);
-    }
-    else {
-      zip.generateAsync({type: 'blob'})
-      .then(content => {
-        const time = new Date();
-        let filename = tab.title += ' ' + time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
-        filename = filename
-          .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>{}[\]\\/]/gi, '-');
-        filename += '.zip';
-        const url = URL.createObjectURL(content);
-        chrome.downloads.download({
-          url,
-          filename: request.custom ? request.custom + '/' + filename : filename,
-          conflictAction: 'uniquify',
-          saveAs: request.saveAs
-        }, () => {
-          chrome.tabs.sendMessage(tab.id, {
-            cmd: 'close-me'
-          });
-          window.setTimeout(() => URL.revokeObjectURL(url), 10000);
-        });
-      });
-    }
-  }
-
-  return (_request, _tab) => {
-    request = _request;
-    tab = _tab;
-    jobs = request.images;
-    zip = new JSZip();
-    one();
-  };
-})();
+  });
+};
 
 chrome.runtime.onConnect.addListener(port => {
   let links = [];
@@ -353,7 +355,8 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
       title: 'Save all Images',
       message: 'Saving ' + request.images.length + ' images'
     });
-    download(request, sender.tab);
+    const download = new Download();
+    download.init(request, sender.tab);
   }
   //
   if (request.cmd === 'close-me') {
