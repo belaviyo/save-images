d6f6f252fefb223aacb5259164fac1e2d267dfef
Joe Ertaba <belaviyo@gmail.com>
2023-03-12 01:40:47 -0500

    fixes #80

--- v2/data/collector.js
+++ v2/data/collector.js
@@ -35,6 +35,17 @@ var collector = {
       skip
     }, callback);
   },
+  findRoots(doc, list = []) {
+    for (const e of doc.querySelectorAll('*')) {
+      if (e.shadowRoot) {
+        try {
+          collector.findRoots(e.shadowRoot, list);
+          list.push(e.shadowRoot);
+        }
+        catch (e) {}
+      }
+    }
+  },
   inspect: img => new Promise((resolve, reject) => {
     const src = img.src.toLowerCase();
     // for known types, force the file extension; also on partial-accurate mode, do not fetch the header
@@ -111,14 +122,14 @@ var collector = {
         src = src.split('#')[0];
 
         // fix src
-        if (src && (src.startsWith('http') === false && src.startsWith('data:') === false) && img.page) {
+        if (src && (src.startsWith('http') === false && src.startsWith('data:') === false && src.startsWith('blob:') === false) && img.page) {
           try {
             img.src = src = (new URL(src, img.page)).href;
           }
           catch (e) {}
         }
 
-        if (src && (src.startsWith('http') || src.startsWith('ftp') || src.startsWith('data:'))) {
+        if (src && (src.startsWith('http') || src.startsWith('ftp') || src.startsWith('data:') || src.startsWith('blob:'))) {
           if (collector.cache[src] === undefined) {
             collector.cache[src] = true;
             if (regexps && regexps.some(r => r.test(src)) === false) {
@@ -170,122 +181,141 @@ var collector = {
         }
       });
     };
-    // find images; part 1/3
-    let images = [];
 
-    for (const img of document.images) {
-      images.push({
-        width: img.width,
-        height: img.height,
-        src: img.currentSrc || img.src,
-        alt: img.alt,
-        custom: img.getAttribute(window.custom) || '',
-        verified: true, // this is an image even if content-type cannot be resolved,
-        page: location.href
-      });
-      if (img.src && img.currentSrc !== img.src) {
+    let images = [];
+    const docs = [document];
+    collector.findRoots(document, docs);
+
+    // find images; part 1/3
+    for (const doc of docs) {
+      for (const img of (doc.images || doc.querySelectorAll('img'))) {
         images.push({
-          src: img.src,
+          width: img.width,
+          height: img.height,
+          src: img.currentSrc || img.src,
           alt: img.alt,
           custom: img.getAttribute(window.custom) || '',
           verified: true, // this is an image even if content-type cannot be resolved,
           page: location.href
         });
+        if (img.src && img.currentSrc !== img.src) {
+          images.push({
+            src: img.src,
+            alt: img.alt,
+            custom: img.getAttribute(window.custom) || '',
+            verified: true, // this is an image even if content-type cannot be resolved,
+            page: location.href
+          });
+        }
       }
     }
     // find images; part 2/3
-    for (const source of document.querySelectorAll('source')) {
-      try {
-        const href = (source.srcset || '').split(' ')[0];
-        images.push({
-          src: (new URL(href, location.href)).href,
-          page: location.href
-        });
-      }
-      catch (e) {
-        console.warn('Cannot collect source images', e);
+    for (const doc of docs) {
+      for (const source of doc.querySelectorAll('source')) {
+        try {
+          const href = (source.srcset || '').split(' ')[0];
+          images.push({
+            src: (new URL(href, location.href)).href,
+            page: location.href
+          });
+        }
+        catch (e) {
+          console.warn('Cannot collect source images', e);
+        }
       }
     }
     // find embedded images on SVG elements; part 3/3
-    for (const image of [...document.querySelectorAll('image')]) {
-      images.push({
-        src: image.href?.baseVal,
-        alt: image.alt,
-        custom: image.getAttribute(window.custom) || '',
-        // if image is verified, we dont have the image size. on accurate mode set it to false
-        verified: true,
-        page: location.href
-      });
-    }
-    images.push(...[...document.querySelectorAll('source')].filter(i => i.srcset).map(i => ({
-      src: i.srcset.split(' ')[0],
-      page: location.href
-    })));
-
-    // find background images; part 2
-    try {
-      [...document.querySelectorAll('*')]
-        .map(e => [
-          getComputedStyle(e).backgroundImage,
-          getComputedStyle(e, ':before').backgroundImage,
-          getComputedStyle(e, ':after').backgroundImage
-        ])
-        .flat()
-        .map(i => {
-          const e = /url\(['"]([^)]+)["']\)/.exec(i);
-          return e && e.length ? e[1] : null;
-        }).filter(s => s).forEach(src => {
-          if (src.startsWith('//')) {
-            src = document.location.protocol + src;
-          }
-          else if (src.startsWith('/')) {
-            document.location.origin + src;
-          }
-          images.push({
-            src,
-            page: location.href
-          });
-        });
-    }
-    catch (e) {
-      console.warn('Cannot collect background images', e);
-    }
-    // find SVGs
-    try {
-      for (const svg of document.querySelectorAll('svg')) {
-        const e = svg.cloneNode(true);
-        e.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-
+    for (const doc of docs) {
+      for (const image of [...doc.querySelectorAll('image')]) {
         images.push({
-          src: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(e.outerHTML),
+          src: image.href?.baseVal,
+          alt: image.alt,
+          custom: image.getAttribute(window.custom) || '',
+          // if image is verified, we dont have the image size. on accurate mode set it to false
+          verified: true,
           page: location.href
         });
       }
     }
-    catch (e) {
-      console.warn('Cannot collect SVG images', e);
+    for (const doc of docs) {
+      images.push(...[...doc.querySelectorAll('source')].filter(i => i.srcset).map(i => ({
+        src: i.srcset.split(' ')[0],
+        page: location.href
+      })));
+    }
+
+    // find background images; part 2
+    for (const doc of docs) {
+      try {
+        [...doc.querySelectorAll('*')]
+          .map(e => [
+            getComputedStyle(e).backgroundImage,
+            getComputedStyle(e, ':before').backgroundImage,
+            getComputedStyle(e, ':after').backgroundImage
+          ])
+          .flat()
+          .map(i => {
+            const e = /url\(['"]([^)]+)["']\)/.exec(i);
+            return e && e.length ? e[1] : null;
+          }).filter(s => s).forEach(src => {
+            if (src.startsWith('//')) {
+              src = document.location.protocol + src;
+            }
+            else if (src.startsWith('/')) {
+              document.location.origin + src;
+            }
+            images.push({
+              src,
+              page: location.href
+            });
+          });
+      }
+      catch (e) {
+        console.warn('Cannot collect background images', e);
+      }
+    }
+    // find SVGs
+    for (const doc of docs) {
+      try {
+        for (const svg of doc.querySelectorAll('svg')) {
+          const e = svg.cloneNode(true);
+          e.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+          images.push({
+            src: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(e.outerHTML),
+            page: location.href
+          });
+        }
+      }
+      catch (e) {
+        console.warn('Cannot collect SVG images', e);
+      }
     }
 
     // find linked images; part 3
-    if (window.deep > 0) {
-      [...document.querySelectorAll('a')].map(a => a.href)
-        .forEach(src => images.push({
-          src,
-          page: location.href
-        }));
+    for (const doc of docs) {
+      if (window.deep > 0) {
+        [...doc.querySelectorAll('a')].map(a => a.href)
+          .forEach(src => images.push({
+            src,
+            page: location.href
+          }));
+      }
     }
     // find hard-coded links; part 4
-    if (window.deep > 0) {
-      const r = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
-      const content = document.documentElement.innerHTML + '\n' + document.body?.textContent;
+    for (const doc of docs) {
+      if (window.deep > 0) {
+        const r = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
+        const content = (doc.documentElement?.innerHTML || '') + '\n' + (doc.body?.textContent || '');
 
-      // decode html special characters; &amp;
-      (content.match(r) || [])
-        .map(s => s.replace(/&amp;/g, '&'))
-        .forEach(src => images.push({
-          src,
-          page: location.href
-        }));
+        // decode html special characters; &amp;
+        (content.match(r) || [])
+          .map(s => s.replace(/&amp;/g, '&'))
+          .forEach(src => images.push({
+            src,
+            page: location.href
+          }));
+      }
     }
     // clean
     images = cleanup(images);
